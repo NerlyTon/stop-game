3d7eb42fd615f1e1e9da302e9495153db4662ed4
NerlyTon <nerlyton@gmail.com>
2020-12-12 19:55:47 -0600

    added index and create method to broadcast

--- stop-backend/app/controllers/players_controller.rb
+++ stop-backend/app/controllers/players_controller.rb
@@ -1,2 +1,23 @@
 class PlayersController < ApplicationController
+
+    def index
+        players = Player.all
+        render json: players
+    end
+
+    def create
+        player = Player.new(player_params)
+
+        if player.save
+            serialized_data = ActiveModelSerializers::Adapter::Json.new(PlayerSerializer.new(player)).serializable_hash,
+            ActionCable.server.broadcast 'players_channel', serialized_data
+            head :ok
+        end
+    end
+
+    private
+
+    def player_params
+        params.require(:player).permit(:id, :initials, :name, :place, :color, :animal, :thing)
+    end
 end
