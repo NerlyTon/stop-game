01d922eeae462c03abb0da9d04b541e12a71968e
NerlyTon <nerlyton@gmail.com>
2020-12-12 23:05:03 -0600

    reducer for the fetch call

---
+++ stop-frontend/src/redux/reducers/playerReducer.js
@@ -0,0 +1,10 @@
+function playerReducer(state = {all: []}, action) {
+    switch(action.type) {
+        case "FETCH_PLAYERS_SUCCESS" :
+            return { ...state, all: action.payload };
+        default:
+            return state;
+    }
+}
+
+export default playerReducer
