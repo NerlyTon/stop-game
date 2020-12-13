eefc4bfad91976797173f5f6395bfd78fdb17b77
NerlyTon <nerlyton@gmail.com>
2020-12-12 23:06:46 -0600

    added the fetch and the channel connetion

--- stop-frontend/src/components/Home.js
+++ stop-frontend/src/components/Home.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import { getPlayers } from '../redux/actions/playerActions'
+import PlayerAnswers from './PlayerAnswers'
+import { ActionCableConsumer } from 'react-actioncable-provider';
 
-export default class Home extends Component {
+
+class Home extends Component {
+
+    state = {
+        players: [],
+      };
+
+
+    componentDidMount() {
+        this.props.getPlayers()
+    }
+
+    handleReceivedPlayers = response => {
+        const { player } = response;
+        this.setState({
+          players: [...this.state.players, player]
+        });
+        console.log("handlereceived players in home", this.state)
+    };
+
+
+
     render() {
+        console.log("I am inside Home Render")
         return (
             <div>
                 <h1>Game</h1>
+                <ActionCableConsumer
+                channel={{ channel: 'PlayersChannel' }}
+                onReceived={this.handleReceivedPlayers}
+                />
+                <h2>Players Answers</h2>
+                <ul>{this.props.players.map(player => <PlayerAnswers player={player}/>)}</ul>
+                
             </div>
         )
     }
 }
+
+const mapStateToProps = ({ players }) => {
+    return {
+        players: players.all
+    }
+}
+
+export default connect(mapStateToProps, { getPlayers })(Home)
