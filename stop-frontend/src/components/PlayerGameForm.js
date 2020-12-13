40e9e681c7cd72e3540df8637a4a0fe2768ffb37
NerlyTon <nerlyton@gmail.com>
2020-12-13 00:11:02 -0600

    handle change on input

--- stop-frontend/src/components/PlayerGameForm.js
+++ stop-frontend/src/components/PlayerGameForm.js
@@ -10,17 +10,22 @@ export default class PlayerGameForm extends Component {
         animal: "",
         thing: ""
     }
+
+    // handleChange = e => {
+    //     this.setState({ initials: e.target.value });
+    //     debugger
+    //   };
     
     render() {
         return (
             <div>
                 <form>
-                    Initials:<input type="text" value={this.state.initials}/><br/>
-                    Name:<input type="text" value={this.state.name}/><br/>
-                    Place:<input type="text" value={this.state.place}/><br/>
-                    Color:<input type="text" value={this.state.color}/><br/>
-                    Animal:<input type="text" value={this.state.animal}/><br/>
-                    Thing:<input type="text" value={this.state.thing}/><br/>
+                    Initials:<input onChange={(e) => this.setState({ initials: e.target.value })} type="text" value={this.state.initials}/><br/>
+                    Name:<input onChange={(e) => this.setState({ name: e.target.value })} type="text" value={this.state.name}/><br/>
+                    Place:<input onChange={(e) => this.setState({ place: e.target.value })} type="text" value={this.state.place}/><br/>
+                    Color:<input onChange={(e) => this.setState({ color: e.target.value })} type="text" value={this.state.color}/><br/>
+                    Animal:<input onChange={(e) => this.setState({ animal: e.target.value })} type="text" value={this.state.animal}/><br/>
+                    Thing:<input onChange={(e) => this.setState({ thing: e.target.value })} type="text" value={this.state.thing}/><br/>
                     <input type="submit" value="STOP"/>
                 </form>
             </div>
