1a8d7d6f77cc3b5ab2352de2501246af9a8da530
NerlyTon <nerlyton@gmail.com>
2020-12-12 20:52:58 -0600

    added scores component

---
+++ stop-frontend/src/components/Scores.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Scores extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Scores</h1>
+            </div>
+        )
+    }
+}
