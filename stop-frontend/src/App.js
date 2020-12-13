45adb671872ffd36f96d54f36f8eb86683ed6cdb
NerlyTon <nerlyton@gmail.com>
2020-12-12 20:46:57 -0600

    wrapped the game with actioncable provider

--- stop-frontend/src/App.js
+++ stop-frontend/src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { ActionCableProvider } from 'react-actioncable-provider';
+import { API_WS_ROOT } from './constants';
 import Home from './components/Home'
 import Rules from './components/Rules'
 import Scores from './components/Scores'
@@ -9,6 +11,7 @@ import Scores from './components/Scores'
 function App() {
   return (
     <div className="App">
+      <ActionCableProvider url={API_WS_ROOT}>
       <Router>
         <Switch>
           <Route exact path="/" component={Home}/>
@@ -16,7 +19,7 @@ function App() {
           <Route exact path="scores" component={Scores}/>
         </Switch>
       </Router>
-      
+      </ActionCableProvider>
     </div>
   );
 }
