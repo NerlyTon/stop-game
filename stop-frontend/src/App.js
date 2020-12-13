799ed42b08fec0044d3549fb5641679446942d14
NerlyTon <nerlyton@gmail.com>
2020-12-12 20:36:46 -0600

    set up routes

--- stop-frontend/src/App.js
+++ stop-frontend/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import Rules from './components/Rules'
+import Scores from './components/Scores'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="rules" component={Rules}/>
+          <Route exact path="scores" component={Scores}/>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
