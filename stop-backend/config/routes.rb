e474d57e90738b7654e9fe926be6c3130290a92f
NerlyTon <nerlyton@gmail.com>
2020-12-12 19:40:31 -0600

    added action cable to routes

--- stop-backend/config/routes.rb
+++ stop-backend/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :games
   resources :players
+
+  # to define the Websocket end point
+  mount ActionCable.server => '/cable'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
