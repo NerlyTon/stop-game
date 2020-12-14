02bb10f297cc7edb271502cf763e759e7f946f69
NerlyTon <nerlyton@gmail.com>
2020-12-14 13:18:29 -0600

    Wrote the rules

--- stop-frontend/src/components/Rules.js
+++ stop-frontend/src/components/Rules.js
@@ -4,6 +4,21 @@ export default function Rules() {
     return (
         <div>
             <h1>Rules</h1>
+            <h3>Welcome to the STOP Game!</h3>
+            <h5>Are you faster than your opponent?</h5>
+            <p>You will receive a Random letter 
+                and when the diplay says start you and 
+                your opponent will fill out the categories 
+                with a word that begins with that specific letter. 
+                Once all the catergories are filled out then you
+                are able to click STOP! Who ever clicks STOP
+                first wins!</p>
+            <h4>Guidelines for Each Category</h4>
+            <p>Initials: This is where you can put your initials (or username).</p>
+            <p>Name: First Name or Last Name.</p>
+            <p>Place: City, State, Country</p>
+            <p>Color: any color and be prepared to show proof to your opponent!</p>
+            <p>Animal: Any animal </p>
         </div>
     )
 }
