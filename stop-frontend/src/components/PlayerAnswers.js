38bfbd76bba84b53bdea16e97f85889dee5e1501
NerlyTon <nerlyton@gmail.com>
2020-12-14 13:20:42 -0600

    removed the key=

--- stop-frontend/src/components/PlayerAnswers.js
+++ stop-frontend/src/components/PlayerAnswers.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PlayerAnswers({ player }) {
     return (
         
-        <li key={player.id}> {player.initials} - {player.name} - {player.place} - {player.color} - {player.animal} - {player.thing}</li> 
+        <li> {player.initials} - {player.name} - {player.place} - {player.color} - {player.animal} - {player.thing}</li> 
 
         
     )
