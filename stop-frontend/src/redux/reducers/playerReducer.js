e7e57eca8a15bc0968236becfd74b767f4ed5493
NerlyTon <nerlyton@gmail.com>
2020-12-14 21:11:08 -0600

    added the create player reducer

--- stop-frontend/src/redux/reducers/playerReducer.js
+++ stop-frontend/src/redux/reducers/playerReducer.js
@@ -2,8 +2,13 @@ function playerReducer(state = {all: []}, action) {
     switch(action.type) {
         case "FETCH_PLAYERS_SUCCESS" :
             return { ...state, all: action.payload };
+        
+
+        case "CREATE_PLAYER_SUCCESS":
+            return { ...state, all: [...state.all, action.payload] };
         default:
             return state;
+  
     }
 }
 
