910099749081a250c922a81a4a7372bf1c82df39
NerlyTon <nerlyton@gmail.com>
2020-12-16 19:31:18 -0600

    changed some configurations to store

--- stop-frontend/src/index.js
+++ stop-frontend/src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/store'
 import { Provider } from 'react-redux'
+import PlayerReducer from './redux/reducers/playerReducer'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store =  createStore(PlayerReducer,composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
