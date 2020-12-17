d0f3456c4ea92f6bb5d486f0860613f8acb496cd
NerlyTon <nerlyton@gmail.com>
2020-12-16 19:29:55 -0600

    added send time action and catch on fetch

--- stop-frontend/src/redux/actions/playerActions.js
+++ stop-frontend/src/redux/actions/playerActions.js
@@ -10,21 +10,56 @@ export const getPlayers = () => {
     }
 }
 
-export const createPlayer = (data) => {
-  console.log("inside create action")
+// export const createPlayer = (data) => {
+//   // console.log("inside create action")
+//     return (dispatch) => {
+//         fetch(`http://localhost:3000/players`, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ player: data }),
+        
+//       })
+      
+//         .then((res) => res.json())
+//         .then((player) =>
+//           dispatch({ type: "CREATE_PLAYER_SUCCESS", payload: player })
+//         );
+        
+//     };
+    
+
+//   };
+
+  export const createPlayer = (data) => {
     return (dispatch) => {
-        fetch(`${API_ROOT}/players`, {
+      return fetch(`http://localhost:3000/players`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ player: data }),
-        
+        body: JSON.stringify({ player: data })
       })
-      
-        // .then((res) => res.json())
-        // .then((player) =>
-        //   dispatch({ type: "CREATE_PLAYER_SUCCESS", payload: player })
-        // );
+        .then((res) => res.json())
+        .then((player) =>
+          dispatch({ type: "CREATE_PLAYER_SUCCESS", payload: player })
+        )
+        .catch((error) => console.log(error))
+        
     };
   };
+
+  export const sendTime = (time) => {
+    return dispatch => {
+      // debugger
+      dispatch({ type: "SEND_TIME", payload: time })
+    }
+  }
+
+  // export const player = (player) => {
+  //   return dispatch => {
+  //     // debugger
+  //     dispatch({ type: "PLAYER_INFO", payload: player })
+  //   }
+  // }
