b52ee9aef25ec3b665c02a6765dc2ab3908050b0
NerlyTon <nerlyton@gmail.com>
2020-12-16 19:30:45 -0600

    added time prop to display

--- stop-frontend/src/components/PlayerAnswers.js
+++ stop-frontend/src/components/PlayerAnswers.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
-export default function PlayerAnswers({ player }) {
+
+class PlayerAnswers extends Component {
+   
+    render() {
+        console.log(this.props.time)
     return (
-        
-        <li> {player.initials} - {player.name} - {player.place} - {player.color} - {player.animal} - {player.thing}</li> 
-
-        
+        <li> {this.props.player.initials} - {this.props.player.name} - {this.props.player.place} - {this.props.player.color} - {this.props.player.animal} - {this.props.player.thing} - {this.props.time.currentTime}</li> 
+  
     )
+}}
+
+const mapStateToProps = (state) => {
+    return {time: state.time}
 }
+
+export default connect(mapStateToProps)(PlayerAnswers)
