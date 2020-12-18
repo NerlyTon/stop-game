d372772e4902192f7482f807a127f14dddcf4dba
NerlyTon <nerlyton@gmail.com>
2020-12-17 20:37:48 -0600

    created players are added into the store

--- stop-frontend/src/components/Game.js
+++ stop-frontend/src/components/Game.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import { getPlayers } from '../redux/actions/playerActions'
-// import { createPlayer } from '../redux/actions/playerActions'
+import { getPlayers, playerAns } from '../redux/actions/playerActions'
 import PlayerAnswers from './PlayerAnswers'
 import { ActionCableConsumer } from 'react-actioncable-provider';
-// import PlayerGameForm from './PlayerGameForm'
+
 import Timer from './Timer';
-// import PlayerGameForm from './PlayerGameForm';
-// import Timer from './Timer';
-// import LetterRandomizer from './LetterRandomizer';
-// import Timer from './Timer';
+import Scores from './Scores';
+
 
 
 
@@ -18,18 +15,108 @@ class Game extends Component {
     
     state = {
         players: [],
+        timerStarted: false,
+        timerStopped: true,
+        minutes: 0,
+        seconds: 0,
+        currentTime: 0,
       };
 
 
-    // componentDidMount() {
-    //     this.props.getPlayers()
-    // }
+    componentDidMount() {
+        this.props.getPlayers()
+    }
 
     handleReceivedPlayers = (players)=> {
         // debugger
         // console.log("here", players)
         const player = players.player
-        this.state.players.push(player)
+        // this.setState({
+        //     players: [...this.state.players, player]
+        // })
+        this.props.playerAns(player)
+        console.log("inside the handle", players)
+        
+       };
+
+       infoFromChild = (info) => {
+        this.setState({
+        // timerStarted: info.timerStarted,
+        // timerStopped: info.timerStopped,
+        // minutes: info.minutes,
+        // seconds: info.seconds,
+        currentTime: info.currentTime,
+        })
+        // debugger
+        console.log(this.state.currentTime)
+       }
+    
+      
+    handleTimer = () => {
+        // App.cable.subscriptions.subscriptions.receive( { data })
+        // debugger
+        console.log("Timer")
+        // this.timer()
+        // return (
+        // <Timer/>)
+    }
+
+
+    render() {
+
+        console.log(this.state, "state")
+        console.log(this.props.playerA, "redux")
+        return (
+            <div>
+                <h1>Word Combat</h1>
+                <h4>Also knows as "STOP paper game"</h4><br/><br/>
+                
+                <ActionCableConsumer
+                channel={{ channel: 'PlayersChannel' }}
+                onReceived={this.handleReceivedPlayers}
+                // onConnected={this.handleTimer}
+                />
+                {/* <button>Start Game</button> */}
+                
+                <Timer fromParent={this.infoFromChild}/>
+                {/* <PlayerGameForm/> */}
+                <h2>Players Answers</h2>
+                {/* {this.checkingToggle()} */}
+            
+                {this.props.playerA.map((player) => {
+               return <PlayerAnswers player={player} key={player.id} state={this.state.info}/>
+                })}
+
+                <Scores/>
+                
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (players) => {
+    console.log("inside map", players)
+    return {
+        players: players.allPlayers, playerA: players.playerAns
+    }
+}
+
+export default connect(mapStateToProps, { getPlayers, playerAns })(Game)
+
+
+            // e.preventDefault()
+            
+
+        // this.setState({toggle: true, players: this.state.players.push(player)})
+        // this.setState(prevState => {
+        //     return {
+        //        players: [...prevState.players, player]
+        //     }
+        //  });
+
+
+//    this.props.player -- 
+        // this.state.players.push(player)
         // this.props.getPlayers({
         //     players: [ player]
         //   });
@@ -41,7 +128,7 @@ class Game extends Component {
         // debugger
         // console.log("handlereceived players in home", this.props)
        
-    };
+    // };
 
     // mapPlayers = (players) => {
     //     return players.map(player => {
@@ -60,48 +147,12 @@ class Game extends Component {
     //     return <Timer/>
 
     // }
-      
-    handleTimer = () => {
-        // App.cable.subscriptions.subscriptions.receive( { data })
-        // debugger
-        console.log("Timer")
-        // this.timer()
-        // return (
-        // <Timer/>)
-    }
 
-
-    render() {
-        console.log("inside game render")
-        return (
-            <div>
-                <h1>Word Combat</h1>
-                <h4>Also knows as "STOP paper game"</h4><br/><br/>
-                
-                <ActionCableConsumer
-                channel={{ channel: 'PlayersChannel' }}
-                onReceived={this.handleReceivedPlayers}
-                // onConnected={this.handleTimer}
-                />
-                {/* <button>Start Game</button> */}
-                
-                <Timer/>
-                {/* <PlayerGameForm/> */}
-                <h2>Players Answers</h2>
-                {this.props.players.map((player) => (
-                <PlayerAnswers player={player} key={player.id} />
-                ))}
-                
-            </div>
-        )
-    }
-}
-
-const mapStateToProps = (players) => {
-    console.log(players)
-    return {
-        players: players.allPlayers
-    }
-}
-
-export default connect(mapStateToProps, { getPlayers})(Game)
+    // checkingToggle = () => {
+    //     if(this.state.toggle) {
+    //     return this.props.players.map((player) => {
+    //            return <PlayerAnswers player={player} key={player.id} />
+    //     })
+    //     }
+    // }
+    
