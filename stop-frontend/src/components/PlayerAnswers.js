4284cbf26697b78219467db4951d6273c4853af0
NerlyTon <nerlyton@gmail.com>
2020-12-17 20:35:45 -0600

    grabed playr info from store and added delete func

--- stop-frontend/src/components/PlayerAnswers.js
+++ stop-frontend/src/components/PlayerAnswers.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
+import { deletePlayer } from '../redux/actions/playerActions'
 
 
 class PlayerAnswers extends Component {
    
+    deleteP = () => {
+        console.log("delete")
+        deletePlayer(this.props.player.id)
+    }
+
+
     render() {
         console.log(this.props.time)
     return (
-        <li> {this.props.player.initials} - {this.props.player.name} - {this.props.player.place} - {this.props.player.color} - {this.props.player.animal} - {this.props.player.thing} - {this.props.time.currentTime}</li> 
+        <li> {this.props.player.initials} - {this.props.player.name} - {this.props.player.place} - {this.props.player.color} - {this.props.player.animal} - {this.props.player.thing} - {this.props.info} <button onClick={this.deleteP} style={{ cursor: "pointer"}}>Delete</button></li> 
   
     )
 }}
@@ -16,4 +23,7 @@ const mapStateToProps = (state) => {
     return {time: state.time}
 }
 
-export default connect(mapStateToProps)(PlayerAnswers)
+export default connect(mapStateToProps, { deletePlayer })(PlayerAnswers)
+
+// {this.props.time ? this.props.time.currentTime: 0}
+
