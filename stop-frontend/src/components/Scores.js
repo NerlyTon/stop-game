6647bc306c99dc375d7f98799f382fbe3ee2cda1
NerlyTon <nerlyton@gmail.com>
2020-12-17 20:34:23 -0600

    started the score logic

--- stop-frontend/src/components/Scores.js
+++ stop-frontend/src/components/Scores.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
+import { playerAns } from '../redux/actions/playerActions'
+import {connect} from 'react-redux'
 
-export default class Scores extends Component {
+
+class Scores extends Component {
+    state = {
+        score: 0
+    }
+
+    // mapOverPlayers = () => {
+    //    return this.props.playerA.map(player => {
+    //         console.log("inside of scores", player)
+    //         if(player.initials === player.initials) {
+    //             this.setState({score: +1})
+    //             return this.state.score
+    //         }
+    //     })
+    // }
+
+
     render() {
         return (
             <div>
                 <h1>Scores</h1>
+                {/* {this.mapOverPlayers()} */}
+                {/* {this.props.playerA.map((player) => {
+                    // console.log("inside score", player)
+                    // debugger
+               return <ul><li key={player.id}>{player.intitials}</li></ul>
+                })} */}
             </div>
         )
     }
 }
+
+const mapStateToProps = (players) => {
+    console.log("inside map", players)
+    return {
+        playerA: players.playerAns
+    }
+}
+
+export default connect(mapStateToProps, { playerAns })(Scores)
