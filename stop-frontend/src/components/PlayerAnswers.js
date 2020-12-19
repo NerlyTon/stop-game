33a5369d30dc8986c33934c43bc48e641c09937d
NerlyTon <nerlyton@gmail.com>
2020-12-19 12:24:25 -0600

    made component to stateless

--- stop-frontend/src/components/PlayerAnswers.js
+++ stop-frontend/src/components/PlayerAnswers.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react'
-import {connect} from 'react-redux'
-import { getPlayer, deletePlayer } from '../redux/actions/playerActions'
 import history from '../history';
 
 
 class PlayerAnswers extends Component {
    
-    deleteP = () => {
-        console.log("delete")
-        this.props.deletePlayer(this.props.player.id)
-    }
 
     handleClickShow = () => {
         history.push(`/answer/${this.props.player.id}`)
@@ -17,18 +11,14 @@ class PlayerAnswers extends Component {
 
 
     render() {
-        console.log("inside p.a", this.props.time)
     return (
-        <li> {this.props.player.initials} - {this.props.player.name} - {this.props.player.place} - {this.props.player.color} - {this.props.player.animal} - {this.props.player.thing} - {this.props.info} <button onClick={this.deleteP} style={{ cursor: "pointer"}}>Delete</button><button onClick={this.handleClickShow}>Look at Answer</button></li> 
+        <li> {this.props.player.initials} - {this.props.player.name} - {this.props.player.place} - {this.props.player.color} - {this.props.player.animal} - {this.props.player.thing} - {this.props.info} <button onClick={() => this.props.sendFunc(this.props.player.id)} style={{ cursor: "pointer"}}>Delete</button><button onClick={this.handleClickShow}>Look at Answer</button></li> 
   
     )
 }}
 
-const mapStateToProps = (state) => {
-    return {time: state.time}
-}
 
-export default connect(mapStateToProps, { getPlayer, deletePlayer })(PlayerAnswers)
+export default (PlayerAnswers)
 
-// {this.props.time ? this.props.time.currentTime: 0}
+
 
