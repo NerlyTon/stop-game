ce7a0771df7f7fad0adde4c76ba764acbe35c340
NerlyTon <nerlyton@gmail.com>
2020-12-18 22:22:30 -0600

    Updated the Rules

--- stop-frontend/src/components/Rules.js
+++ stop-frontend/src/components/Rules.js
@@ -4,21 +4,27 @@ export default function Rules() {
     return (
         <div>
             <h1>Rules</h1>
-            <h3>Welcome to the STOP Game!</h3>
+            <h3>Welcome to WORD COMBAT game</h3>
             <h5>Are you faster than your opponent?</h5>
-            <p>You will receive a Random letter 
-                and when the diplay says start you and 
-                your opponent will fill out the categories 
+            <p>You will receive a Random letter and the 
+                timer will begin you and your opponent will 
+                have two minutes to fill out the categories 
                 with a word that begins with that specific letter. 
                 Once all the catergories are filled out then you
-                are able to click STOP! Who ever clicks STOP
-                first wins!</p>
+                are able to click SUBMIT! 
+            </p>
+            <p>If you click SUBMIT before the time is up, then
+                you will receive a new letter. Who ever gets the most words done 
+                <span>WINS!</span>
+            </p>
             <h4>Guidelines for Each Category</h4>
-            <p>Initials: This is where you can put your initials (or username).</p>
-            <p>Name: First Name or Last Name.</p>
-            <p>Place: City, State, Country</p>
-            <p>Color: any color and be prepared to show proof to your opponent!</p>
-            <p>Animal: Any animal </p>
+            <p>Initials: This is where you can put your initials (or username)</p>
+            <p>Name: First Name or Last Name</p>
+            <p>Place: City, State, or Country</p>
+            <p>Color: Any color and be prepared to show proof to your opponent!</p>
+            <p>Animal: Any Animal </p>
+
+            <h1>To begin click on the GAME link Above!</h1>
         </div>
     )
 }
