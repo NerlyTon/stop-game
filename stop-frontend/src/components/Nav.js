30fdc92491d3ccf646661ed7f217117defe1165a
NerlyTon <nerlyton@gmail.com>
2020-12-18 22:00:41 -0600

    removed list tag

--- stop-frontend/src/components/Nav.js
+++ stop-frontend/src/components/Nav.js
@@ -5,11 +5,10 @@ import {Link} from 'react-router-dom'
 export default function Nav() {
     return (
         <div>
-            <ul>
-                <li><Link to='/'>Game</Link></li>
-                <li><Link to='/rules'>Rules</Link></li>
-                <li><Link to='/scores'>Scores</Link></li>
-            </ul>
+            
+                <Link to='/'>Game</Link><br/>       
+                <Link to='/rules'>Rules</Link>
+            
         </div>
     )
 }
