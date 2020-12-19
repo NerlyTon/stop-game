e79c58ff00855f4e5be0f2f9aaca0e490a0154fc
NerlyTon <nerlyton@gmail.com>
2020-12-19 12:23:27 -0600

    added message need to add styling

--- stop-frontend/src/components/Footer.js
+++ stop-frontend/src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react'
+// import styled, { keyframes } from 'styled-components'
+// import { rollIn } from 'react-animation'
+
+
+// const RollIn = styled.div`animation: 2s ${keyframes `${rollIn}`} infinite`;
 
 export default function Footer() {
     return (
         <div>
-            
+            <h3>Who will win?</h3>
         </div>
     )
 }
