e006f54e78c19b561d64e94c582a99b052f7b07f
NerlyTon <nerlyton@gmail.com>
2020-12-18 21:53:40 -0600

    function for delete and show completed

--- stop-frontend/src/components/PlayerAnswers.js
+++ stop-frontend/src/components/PlayerAnswers.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import { deletePlayer } from '../redux/actions/playerActions'
+import { getPlayer, deletePlayer } from '../redux/actions/playerActions'
+// import Answer from './Answer'
+// import { Redirect } from "react-router-dom";
+import history from '../history';
 
 
 class PlayerAnswers extends Component {
    
     deleteP = () => {
-        console.log("delete")
-        deletePlayer(this.props.player.id)
+        console.log("delet")
+        this.props.deletePlayer(this.props.player.id)
+    }
+
+    viewAns = () => {
+        // debugger
+        // console.log("view")
+        // this.props.getPlayer(this.props.player.id)
+        // <Answer/>
+        // return <Redirect to="/answer" />
+        // this.props.history.push('/answer');
+        history.push(`/answer/${this.props.player.id}`)
     }
 
 
     render() {
         console.log(this.props.time)
     return (
-        <li> {this.props.player.initials} - {this.props.player.name} - {this.props.player.place} - {this.props.player.color} - {this.props.player.animal} - {this.props.player.thing} - {this.props.info} <button onClick={this.deleteP} style={{ cursor: "pointer"}}>Delete</button></li> 
+        <li> {this.props.player.initials} - {this.props.player.name} - {this.props.player.place} - {this.props.player.color} - {this.props.player.animal} - {this.props.player.thing} - {this.props.info} <button onClick={this.deleteP} style={{ cursor: "pointer"}}>Delete</button><button onClick={this.viewAns}>Look at Answer</button></li> 
   
     )
 }}
@@ -23,7 +36,7 @@ const mapStateToProps = (state) => {
     return {time: state.time}
 }
 
-export default connect(mapStateToProps, { deletePlayer })(PlayerAnswers)
+export default connect(mapStateToProps, { getPlayer, deletePlayer })(PlayerAnswers)
 
 // {this.props.time ? this.props.time.currentTime: 0}
 
