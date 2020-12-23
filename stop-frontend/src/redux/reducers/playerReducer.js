c78b5fae0a33f46cde75c2d03fd2411617c08797
NerlyTon <nerlyton@gmail.com>
2020-12-23 13:10:43 -0600

    added loading player and reset reducers

--- stop-frontend/src/redux/reducers/playerReducer.js
+++ stop-frontend/src/redux/reducers/playerReducer.js
@@ -1,10 +1,13 @@
-const initialState = { time: null, allPlayers: [], playerAns: [], answer: {}}
+const initialState = { time: null, allPlayers: [], playerAns: [], answer: {}, requesting: false}
 
 function playerReducer(state = initialState, action) {
     switch(action.type) {
         case "FETCH_PLAYER_SUCCESS" :
-            console.log("inside the fetch player reducer")
-            return {answer: action.payload }
+        //    debugger
+            return {...state, answer: action.payload, requesting: false }
+
+        case "LOADING_PLAYER" :
+            return {...state, requesting: true}
             
         case "CREATE_PLAYER_SUCCESS":
             return { ...state, allPlayers: [...state.allPlayers, action.payload] };
@@ -20,6 +23,10 @@ function playerReducer(state = initialState, action) {
         case "PLAYER_ANS":
             // debugger
             return { ...state, playerAns: [...state.playerAns, action.payload] };
+
+        case "RESET":
+        // debugger
+            return initialState
     
         default:
             return state;
